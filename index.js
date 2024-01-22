6697b85f3f64de4387e1a034e04cc47f9f033def
silas <silasgebhart12@gmail.com>
2024-01-22 13:14:20 -0600

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
+
+function writeCards(names, eventName) {
+  const thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+
+    thankYouMessages.push(message);
+  }
+
+  return thankYouMessages;
+}
